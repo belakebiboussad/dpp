399526a8fedc9f9cc8630cdcb8ff449f39a62652
belakebi <bbelakebi@cdta.dz>
2019-03-21 17:21:46 +0100

    dsfq

--- resources/views/consultations/resume_cons.blade.php
+++ resources/views/consultations/resume_cons.blade.php
@@ -1,4 +1,13 @@
 @extends('app_med')
+@section('style')
+<style>
+	thead th {
+		  font-size: 0.6em;
+		  padding: 1px !important;
+		  height: 15px;
+	}
+</style>
+@endsection
 @section('main-content')
 <div class="page-header">
 <h1><strong>Résumé  du Consultation Pour :</h1>
@@ -20,90 +29,55 @@
 	</div>
 </div>
 <div class="row">
-	<div class="col-sm-6">
-		<div class="table-responsive">
-		<table id="simple-table" class="table table-striped table-bordered table-hover table-condensed">
-		<thead>
-			<tr>
-			 	<th  width="5%">#</th>
-				<th class="text-center" width="15%" ><b  style="font-weight: bold;"><h5>Date</h5></b></th>
-				<th class="text-center" width="35%"><strong> <h5>Motif</h5></strong></th>
-				<th class="text-center" width="15%"><h5>Medecin</h5></th>
-				<th class="text-center" width="20%"><span><h5>Lieu consultation</h5></span> </th>
-				<th  class="text-center" width="10%"></th>
-			</tr>
-		</thead>
-		</table>
-		 <div class="bodycontainer scrollable">
-		 <table class="table table-hover table-striped table-condensed table-scrollable">
-		<tbody>
-		@foreach($consults as $i=>$consult)
-		<tr class="{!! ( $consult->id == $consultation->id) ? 'success' : '' !!}">
-				<td width="5%">{{ ++ $i }}</td>
-				<td class="center" width="15%">
-					<div class="action-buttons">
-						<span>{{$consult->Date_Consultation}}</span>
-					</div>
-				</td>
-				<td class="center" width="35%">
-					<div class="action-buttons" >
-						<span >{{$consult->Motif_Consultation}}</span>
-					</div>
-				</td>
-				<td class="center" width="15%">
-					<div class="action-buttons">
-					<span >
-					{{ $consult->Nom_Employe }} 
-					 {{ $consult->Prenom_Employe }}	
-					</span>
-					</div>
-				</td>
-				<td class="center" width="20%">
-					<div class="action-buttons">
-						<span >{{$consult->Nom}}</span>
-					</div>
-				</td>
-				<td class="center"  width="10%">
-					<div class="action-buttons">
-					<button onclick="showConsult({{ $consults }} ,{{ $i }})"> 
-					 <i class="ace-icon fa fa-pencil bigger-120"></i></button>
-					</div>
-				</td>
-			</tr>
-		@endforeach
-		</tbody>
-		</div>
-		</table>
-		</div>
-		<table class="table table-hover table-striped table-condensed">
-        			<tfoot>
-            			<!-- add your footer here... -->
-        			</tfoot>
-    		</table>
-	</div> {{-- liste consults --}}
-	</div>
-	<div class="col-sm-6">
-	{{-- 	<div class="space-6"></div> --}}
+		<div class="col-sm-7">
 		<div id="accordion" class="accordion-style1 panel-group ">
-		<div class="panel panel-default">
-			<div class="panel-heading">
-				<h4 class="panel-title">
-				<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#Interogatoire" aria-expanded="false">
-				<i class="bigger-110 ace-icon fa fa-angle-right" data-icon-hide="ace-icon fa fa-angle-down" data-icon-show="ace-icon fa fa-angle-right"></i>&nbsp;<h4 style ="display: inline-block;">Interogatoire</h4>
-				</a>
-				</h4>
-			</div>
-			<div class="panel-collapse collapse" id="Interogatoire" aria-expanded="false" style="height: 0px;">				<div class="panel-body">
-				   <label for="histoire_maladie"><span class="bigger-120"><b>Histoire de la maladie:</b></span></label>
-				<textarea type="text" id="histoire_maladie" style="width:100%; height: 10%;" readonly >{{ $consultation->Motif_Consultation }}</textarea>
+	           <div class="row">
+			<div class="col-sm-12">
+			<div class="widget-box">
+			<div class="widget-header" >
+                        		<h4 class="widget-title">
+                        			<font color="black"><strong>Interogatoire</strong></font>                        			
+                        		</h4>
+                        	</div>		
+               		<div class="widget-body">
+				<div class="widget-main">
+					<div class="row">
+						<div class="col-xs-12">
+							<label for="Motif_Consultation"><strong>Motif de la Consultation :</strong></label>
+							<textarea type="text" id="Motif_Consultation" style="width:100%; height: 8%;" readonly >{{ $consultation->Motif_Consultation }}</textarea>
+						</div>
+					</div>{{-- row	 --}}
+					@if(isset($consultation->histoire_maladie))
+					<div class="row">
+						<div class="col-xs-12">
+							<label for="histoire_maladie"><strong>Histoire de la maladie :</strong></label>
+							<textarea type="text" id="histoire_maladie" style="width:100%; height: 8%;" readonly >{{ $consultation->histoire_maladie }}</textarea>
+						</div>
+					</div>
+					@endif
+					@if(isset($consultation->Diagnostic))
+					<div class="row">
+						<div class="col-xs-12">
+							<label for="Diagnostic"><strong>Diagnostic:</strong></label>
+							<textarea type="text" id="Diagnostic" style="width: 100%; height: 8%;" readonly >{{ $consultation->Diagnostic }}</textarea>
+						</div>
+					</div>
+					@endif
+					@if(isset($consultation->Resume_OBS))
+					<div class="row">
+						<div class="col-xs-12">
+							<label for="Resume_OBS"><span class="bigger-120"><b>Résumé:</b></span></label><textarea type="text" id="Resume_OBS" style="width: 100%; height: 10%;" readonly >{{ $consultation->Resume_OBS }}</textarea>
+						</div>
+					</div>
+					@endif
 
-				<label for="Diagnostic"><span class="bigger-120"><b>Diagnostic:</b></span></label>
-				<textarea type="text" id="Diagnostic" style="width: 100%; height: 10%;" readonly >{{ $consultation->Diagnostic }}</textarea>
-				<label for="Resume_OBS"><span class="bigger-120"><b>Résumé:</b></span></label><textarea type="text" id="Resume_OBS" style="width: 100%; height: 10%;" readonly >{{ $consultation->Resume_OBS }}</textarea>
+				</div>{{-- widget-main--}}
+			</div>{{-- widgetbody --}}
+			</div>	{{-- widgetbox --}}
+			</div>	{{-- sm-12 --}}
+		</div>	{{-- row --}}
 
-			</div>
-			</div>
-		</div>
+	           {{-- examenclique --}}
 		@if(isset($exmclin) )
 		<div class="panel panel-default">
 			<div class="panel-heading">
@@ -159,46 +133,44 @@
 			</div>
 		</div>
 		@endif
-		@if(count($examensbios) !=0)
-		<div class="panel panel-default" >
-			<div class="panel-heading">
-				<h4 class="panel-title">
-				<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false"><i class="bigger-110 ace-icon fa fa-angle-right" data-icon-hide="ace-icon fa fa-angle-down" data-icon-show="ace-icon fa fa-angle-right"></i>&nbsp;<h4 style ="display: inline-block;">Examen Biologique</h4>
-				</a>
-				</h4>
-			</div>
-			<div class="panel-collapse collapse" id="collapseTwo" aria-expanded="false">
-			<div class="panel-body">
-			<div class="widget-box widget-color-blue ui-sortable-handle" id="widget-box-2">
-			<div class="widget-body">
-			<div class="widget-main no-padding">
-			<table class="table table-striped table-bordered table-hover">
-				<thead class="thead-dark">
-					<tr>
-						<th class ="text-center">
-						<i class="fa fa-flask deep-purple-text"></i>
-						<span class="bigger-120"><b>Nom examen</b></span> 	
-						</th>					
-						<th class="text-center hidden-480"><span class="bigger-120"><b>Type examen</b></span></th>
-					</tr>
-				</thead>
-				<tbody>
-				@foreach ($examensbios as $exam)
-					<tr>
-					<td class="">{{$exam->nom}}</td>
-					<td>
-					<a href="#">{{$exam->classe}}</a>
-					</td>
-					</tr>
-				@endforeach
-				</tbody>
-			</table>
-			</div>
-			</div>	{{-- widgetbody --}}
-			</div>{{-- widget-box --}}
-			</div>
-			</div>
-		</div>
+		@if(isset($consultation->demandeexmbio))
+		          <div id="friends" class="tab-pane">
+                    			<div>
+		                        <table class="table table-striped table-bordered">
+		                          <thead>
+		                            <tr>
+		                              <th class="center">#</th>
+		                              <th>Date</th>
+		                              <th>Etat</th>
+		                              <th></th>
+		                            </tr>
+		                          </thead>
+		                          <tbody>
+		                          @foreach($consultation->demandeexmbio as $index => $demande)
+		                            <tr>
+		                              <td class="center">{{ $index +1 }}</td>
+		                              <td>{{ $demande->DateDemande }}</td>
+		                              <td>
+		                                @if($demande->etat == "E")
+		                                  En Attente
+		                                @elseif($demande->etat == "V")
+		                                  Validé
+		                                @else
+		                                  Rejeté
+		                                @endif
+		                              </td>
+		                              <td class="center">
+		                                <a href="{{ route('demandeexb.show', $demande->id_demandeexb) }}">
+		                                  <i class="fa fa-eye"></i>
+		                                </a>
+		                              </td>
+		                            </tr>
+		                          @endforeach               
+		                          </tbody>
+		                        </table>
+                     	 </div>
+                    </div><!-- /#friends -->
+                   
 		@endif
 		@if(isset($examensimg)  && count($examensimg) !=0)	
 		<div class="panel panel-default">
@@ -248,7 +220,57 @@
 			</div>
 		</div>
 		@endif
-		@if(isset($medicaments)  && count($medicaments) !=0)
+		@if(isset($ordonnance->medicamentes )  && count($ordonnance->medicamentes ) !=0)
+		<div class="row">
+			<div class="col-sm-12">
+			<div class="widget-box">
+				<div class="widget-header" >
+	                        		<h4 class="widget-title">
+	                        			<i class="orange ace-icon fa fa-medkit bigger-120"></i><font color="black"><strong>Ordonnance</strong></font>    
+	                        		</h4>
+	                        		<div class="widget-toolbar no-border my-right-float">
+	                        			<a href="/showordonnance/{{ $ordonnance->id }}" target="_blank" class="btn btn-primary my-right-float">
+					                     <i class="fa fa-eye"></i>&nbsp;Visualiser Ordonnance
+					           </a>
+	                        		</div>
+	                        	</div>	
+				<div class="widget-body">
+					<div class="widget-main">
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="space-12"></div>		
+								<table class="table table-striped table-bordered">
+		                                      				<thead>
+			                                        				<tr>
+							                                           <th class="center">#</th>
+							                                           <th><strong>Nom</strong></th>
+							                                           <th><strong>Dosage</strong></th>
+							                                           <th><strong>Forme</strong></th>
+							                                           <th><strong>Posologie</strong></th>
+						                                           </tr>
+		                                     				 </thead>
+		                                      				<tbody>
+						                                           @foreach($ordonnance->medicamentes as $index => $med)
+						                                           <tr>
+							                                          <td>{{ $index + 1 }}</td>
+							                                          <td>{{ $med->Nom_com }}</td>
+							                                          <td>{{ $med->Dosage }}</td>
+							                                          <td>{{ $med->Forme }}</td>
+							                                          <td>{{ $med->pivot->posologie }}</td>
+						                                           </tr>
+						                                           @endforeach
+						                                </tbody>
+					                                </table>
+					                           
+					                      </div>
+					           </div>
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>	
+		@endif
+	           {{-- @if(isset($medicaments)  && count($medicaments) !=0)
 		<div class="panel panel-default">
 			<div class="panel-heading">
 				<h4 class="panel-title">
@@ -283,12 +305,72 @@
 				</tbody>
 				</table>
 				</div>
-			</div>	{{-- widgetbody --}}
-			</div>{{-- widget-box --}}
+			</div>widgetbody
+			</div>widgetbox
 			</div>
 		</div>
-		@endif
+		@endif --}}
 	</div>
-	</div> 	{{-- details consult --}}
+	</div> 	{{-- details consult  7--}}
+	<div class="col-sm-5">
+		<div class="table-responsive">
+		{{-- <table id="simple-table" class="table table-striped table-bordered table-hover table-condensed"> --}}
+		<table id="simple-table" class="table table-striped table-bordered table-hover table-condensed">
+			<thead>
+				<tr> 	{{-- <th  width="5%">#</th> --}}
+					<th class="text-center" width="15%"><strong>Date</strong></th>
+					<th class="text-center" width="40%"><strong>Motif</strong></th>
+					<th class="text-center" width="20%"><strong>Medecin</strong></th>
+					<th class="text-center" width="20%" ><strong>Lieu consultation</strong></th>
+					<th  class="text-center" width="5%"></th>
+				</tr>
+			</thead>
+		</table>
+		 <div class="bodycontainer scrollable" style="margin-top: -2.9%;">
+			 <table class="table table-hover table-striped table-condensed table-scrollable">
+				<tbody>
+				@foreach($consults as $i=>$consult)
+					<tr class="{!! ( $consult->id == $consultation->id) ? 'success' : '' !!}">{{-- <td width="5%">{{ ++ $i }}</td> --}}
+						<td class="center" width="15%">
+							<div class="action-buttons">
+								<span>{{$consult->Date_Consultation}}</span>
+							</div>
+						</td>
+						<td class="center" width="40%">
+							<div class="action-buttons" >
+								<span >{{$consult->Motif_Consultation}}</span>
+							</div>
+						</td>
+						<td class="center" width="20%">
+							<div class="action-buttons">
+							<span >
+							{{ $consult->Nom_Employe }} 
+							 {{ $consult->Prenom_Employe }}	
+							</span>
+							</div>
+						</td>
+						<td class="center" width="20%">
+							<div class="action-buttons">
+								<span >{{$consult->Nom}}</span>
+							</div>
+						</td>
+						<td class="center"  width="5%">
+							<div class="action-buttons my-right-float">
+								{{-- <button onclick="showConsult({{ $consult->id }} ,{{ $i }})"> 
+								<i class="fa fa-hand-o-up fa-xs"></i></button> --}}
+							           <a class="btn btn-xs btn-success" href="/consultations/detailcons/{{$consult->id}}">	
+                            							<i class="ace-icon fa fa-hand-o-up fa-xs"></i>
+                           						</a>
+							</div>
+						</td>
+					</tr>
+				@endforeach
+				</tbody>
+			</table>
+		</div>
+		</div>
+	</div> {{-- liste consults --}}
+	</div>
+
 </div>
 @endsection
