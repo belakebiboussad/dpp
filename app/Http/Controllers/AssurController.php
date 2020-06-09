e71e724580d79e98e7906c5b3385c4cc202f2aea
belakebi <bbelakebi@cdta.dz>
2020-06-09 11:08:50 +0100

    d

--- app/Http/Controllers/AssurController.php
+++ app/Http/Controllers/AssurController.php
@@ -63,7 +63,11 @@ class AssurController extends Controller
      */
     public function edit(assur $assur)
     {
+        dd($assur);
         //
+        //   $atcd = antecedant::FindOrFail($id);
+        // $patient = patient::FindOrFail($atcd->Patient_ID_Patient);
+        // return view('antecedents.edit_atcd',compact('atcd','patient'));
     }
 
     /**
@@ -90,14 +94,11 @@ class AssurController extends Controller
     }
     public function search(Request $request)
     {
-
         if($request->ajax())  
         {
             $output="";
             $assures =   assur::where('Matricule', 'like', '%' . request('matricule') . '%')
                               ->where('NSS', 'LIKE', '%' . request('nss') . "%")->get();
-                        
-          
             if($assures)
             {  
                 $i=0; 
@@ -116,7 +117,7 @@ class AssurController extends Controller
                               '<td>'.$sexe.'</td>'.
                               '<td><span class="badge badge-success">'.$assure->Etat.'</span></td>'.
                               // '<td>'.$assure->service->nom.'</td>'.
-                              '<td class="center">'.'<a href="/assur/'.$assure->id.'" class="'.'btn btn-warning btn-xs" data-toggle="tooltip" title="Consulter le dossier" data-placement="bottom"><i class="fa fa-hand-o-up fa-xs"></i>&nbsp;</a>'."&nbsp;&nbsp;".'<a href="/patient/'.$assure->id.'/edit" class="'.'btn btn-info btn-xs" data-toggle="tooltip" title="modifier"><i class="fa fa-edit fa-xs" aria-hidden="true" style="font-size:16px;"></i></a>'.'</td>'.
+                              '<td class="center">'.'<a href="/assur/'.$assure->id.'" class="'.'btn btn-warning btn-xs" data-toggle="tooltip" title="Consulter" data-placement="bottom"><i class="fa fa-hand-o-up fa-xs"></i>&nbsp;</a>'."&nbsp;&nbsp;".'<a href="/patient/'.$assure->id.'/edit" class="'.'btn btn-info btn-xs" data-toggle="tooltip" title="modifier"><i class="fa fa-edit fa-xs" aria-hidden="true" style="font-size:16px;"></i></a>'.'</td>'.
                                '</tr>';
                              
                 }
