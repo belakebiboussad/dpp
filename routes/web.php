69c25d16d090f8eafd44730cfb10e4e842bdbca8
bbelakebi@cdta.dz <bbelakebi@cdta.dz>
2020-09-24 15:30:17 +0100

    ss

--- routes/web.php
+++ routes/web.php
@@ -67,7 +67,6 @@ Route::get('exbio/{filename}', function ($filename)
         return $response;
 });// Auth::routes();
 route::get('/detailsdemande/{id}','demandeprodController@details_demande');
-route::get('/traiterdemande/{id}','demandeprodController@traiter_demande');
 Route::post('user/credentials','UsersController@credentials');
 Route::post('user/updatepro','UsersController@updatepro');
 Route::get('/atcd/store','AntecedantsController@storeatcd');
