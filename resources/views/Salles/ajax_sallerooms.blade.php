9ae93bf3ec24edb05c9b31b8d2f8e0b587d7576d
Fatma Zohra Bouzidi <fbouzidi@cdta.net>
2021-02-03 09:27:15 +0100

    commited 	new file:   resources/views/Salles/ajax_sallerooms.blade.php 	new file:   resources/views/lits/ajax_litrooms.blade.php

---
+++ resources/views/Salles/ajax_sallerooms.blade.php
@@ -0,0 +1,35 @@
+<div class="servive-block servive-block-grey" id="widget-box-2">
+<div class="widget-header">
+	<h5 class="widget-title bigger lighter">
+		<i class="ace-icon fa fa-table"></i>
+		<span><b>Liste des Lis</b></span>
+	</h5>
+</div>
+<div class="widget-body">
+	<div class="widget-main no-padding">
+	<table class="table table-striped table-bordered table-hover">
+		<thead>
+		<tr>
+			<th>Numéro</th>
+			<th>nom </th>
+			<th>Bloqué</th>
+			<th>Affectation</th>
+		</tr>
+		</thead>
+		<tbody >
+		@foreach ($lits as $key=>$lit)
+			<tr>
+				<th>{{$lit->num}}</th>
+				<th>{{$lit->nom}}</th>
+				<th>{{ $lit->etat == 1 ? "Non Bloqué" : "Bloqué" }}</th>
+		    	<th>{{ $lit->affectation == 0 ? "Non Affecté" : "Affecté" }}</th>
+			</tr>
+
+
+		@endforeach
+		<tr></tr>	
+		</tbody>
+				</table>
+				</div>	{{-- widget-main--}}
+			</div>{{-- widget-body --}}
+			</div> 
