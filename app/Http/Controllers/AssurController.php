c981d31d86887419132e0c3ff4877dc9557faa04
belakebi <bbelakebi@cdta.dz>
2021-05-07 13:42:14 +0100

    sdf

--- app/Http/Controllers/AssurController.php
+++ app/Http/Controllers/AssurController.php
@@ -159,8 +159,11 @@ class AssurController extends Controller
      public function destroy(Request $request , $id) 
      { 
         // $handle = new COM("GRH2.Personnel") or die("Unable to instanciate Word"); //dll local//D:/Mes-programmes/DotNET/Dll/GRH2/GRH2
-        $handle = new COM("GRH.Personnel") or die("Unable to instanciate Word"); //vrai derniere dll local//D:\cdta-work\Dossier Patient\DGSN-Glysines\DLL\Mien\Debugs
-        //$handle = new COM("GRH_DLL.Personnel") or die("Unable to instanciate Word");//dgsn network dll
+        //J:\TRAVAIL_CDTA\DossierPatient_Projet_Actuel\DGSN-Dossier PAtient\DLL\!DLL_CDTA_My//I:/MessPrograms/program.net/com/GRH/GRH/bin/Debug/GRH.DLL 
+        //vrai derniere dll local//D:\cdta-work\Dossier Patient\DGSN-Glysines\DLL\Mien\Debug
+        $handle = new COM("GRH.Personnel") or die("Unable to instanciate Word"); 
+        //dgsn network dll,path=J:\TRAVAIL_CDTA\DossierPatient_Projet_Actuel\DGSN-Dossier PAtient\DLL\!Last_DGSN_DLL//I:/MessPrograms/program.net/Pgm-Laptop/GRH_DLL/GRH_DLLL/bin/x64/Debug
+        //$handle = new COM("GRH_DLL.Personnel") or die("Unable to instanciate Word");
         if($handle != null)
         {
           $assure = $handle->SelectPersonnel(trim('g125M'),trim(''));//10246
