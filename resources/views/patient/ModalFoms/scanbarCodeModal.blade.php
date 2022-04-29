d922cce91546d8442a18c349dc9e949baff5e833
belakebi <bbelakebi@cdta.dz>
2022-04-29 07:54:12 +0100

    s

--- resources/views/patient/ModalFoms/scanbarCodeModal.blade.php
+++ resources/views/patient/ModalFoms/scanbarCodeModal.blade.php
@@ -1,49 +1,26 @@
 <div class="modal" id="livestream_scanner">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-				</button>
-				<h4 class="modal-title">Barcode Scanner</h4>
-			</div>
-			<div class="modal-body" style="position: static">
-				<div id="interactive" class="viewport"></div>
-				<div class="error" id ="errorMsg"></div>
-			</div>
-			<div class="modal-footer a">
-			<div class="controls">
-				<fieldset class="reader-config-group">
-          <label>
-            <span>Type</span>
-            <select name="decoder_readers">
-              <option value="code_128" selected="selected">Code 128</option>
-            </select>
-          </label> 
-    			<label>
-            <span>Resolution</span>
-            <select name="input-stream_constraints" class="tt-input input-sx">
-              <option value="320x240">320px</option><option value="640x480">640px</option> <option value="800x600">800px</option>
-              <option selected="selected" value="1280x720">1280px</option>
-              <option value="1600x960">1600px</option><option value="1920x1080">1920px</option>
-            </select>
-          </label>
-      		<label class="hidden-xs">
-            <span>Camera</span>
-            <select name="input-stream_constraints" id="deviceSelection" class="tt-input input-sx">
-            </select>
-          </label>
-         	<label style="display: none">
-          	<span>Zoom</span><select name="settings_zoom" class="tt-input input-sx"></select>	
-			    </label>
-			    <label style="display: none">
-			      <span>Torch</span>
-			      <input type="checkbox" name="settings_torch" class="tt-input input-sx"/>
-			    </label>
-      	</fieldset>
-      </div><!-- controls -->
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <h4 class="modal-title">Barcode Scanner</h4>
+      </div>
+      <div class="modal-body" style="position: static">
+        <div id="interactive" class="viewport"></div>
+        <div class="error" id ="errorMsg"></div>
+      </div>
+      <div class="modal-footer">
+      <div class="controls">
+      <fieldset class="reader-config-group">
+        <label class="hidden-xs">
+          <span>Camera</span>
+          <select name="input-stream_constraints" id="deviceSelection" class=""> </select>
+        </label>
+        </fieldset>
+      </div>
       <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-			</div>
-		</div>
-	</div>
+    </div>
+  </div>
 </div>
