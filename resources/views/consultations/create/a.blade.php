4f5b97cb587299943c5a66708b1f8472cde172fd
belakebi <bbelakebi@cdta.dz>
2022-06-05 09:41:07 +0100

    s

--- resources/views/consultations/create/a.blade.php
+++ resources/views/consultations/create/a.blade.php
@@ -162,7 +162,6 @@
             }
             else error.insertAfter(element.parent());
           },
-    
           submitHandler: function (form) {
           },
           invalidHandler: function (form) {
@@ -186,95 +185,20 @@
 @endsection
 @section('main-content')
 <div class="main-content">
-        <div class="main-content-inner">
-          <div class="breadcrumbs ace-save-state" id="breadcrumbs">
-            <ul class="breadcrumb">
-            </ul><!-- /.breadcrumb -->
-            <div class="nav-search" id="nav-search">
-              <form class="form-search">
-              </form>
-            </div><!-- /.nav-search -->
-          </div>
-          <div class="page-content">
-            <div class="ace-settings-container" id="ace-settings-container">
-              <div class="ace-settings-box clearfix" id="ace-settings-box">
-                <div class="pull-left width-50">
-                  <div class="ace-settings-item">
-                    <div class="pull-left">
-                      <select id="skin-colorpicker" class="hide">
-                        <option data-skin="no-skin" value="#438EB9">#438EB9</option>
-                        <option data-skin="skin-1" value="#222A2D">#222A2D</option>
-                        <option data-skin="skin-2" value="#C6487E">#C6487E</option>
-                        <option data-skin="skin-3" value="#D0D0D0">#D0D0D0</option>
-                      </select><div class="dropdown dropdown-colorpicker">    <a data-toggle="dropdown" class="dropdown-toggle"><span class="btn-colorpicker" style="background-color:#438EB9"></span></a><ul class="dropdown-menu dropdown-caret"><li><a class="colorpick-btn selected" style="background-color:#438EB9;" data-color="#438EB9"></a></li><li><a class="colorpick-btn" style="background-color:#222A2D;" data-color="#222A2D"></a></li><li><a class="colorpick-btn" style="background-color:#C6487E;" data-color="#C6487E"></a></li><li><a class="colorpick-btn" style="background-color:#D0D0D0;" data-color="#D0D0D0"></a></li></ul></div>
-                    </div>
-                    <span>&nbsp; Choose Skin</span>
-                  </div>
-
-                  <div class="ace-settings-item">
-                    <input type="checkbox" class="ace ace-checkbox-2 ace-save-state" id="ace-settings-navbar" autocomplete="off">
-                    <label class="lbl" for="ace-settings-navbar"> Fixed Navbar</label>
-                  </div>
-
-                  <div class="ace-settings-item">
-                    <input type="checkbox" class="ace ace-checkbox-2 ace-save-state" id="ace-settings-sidebar" autocomplete="off">
-                    <label class="lbl" for="ace-settings-sidebar"> Fixed Sidebar</label>
-                  </div>
-
-                  <div class="ace-settings-item">
-                    <input type="checkbox" class="ace ace-checkbox-2 ace-save-state" id="ace-settings-breadcrumbs" autocomplete="off">
-                    <label class="lbl" for="ace-settings-breadcrumbs"> Fixed Breadcrumbs</label>
-                  </div>
-
-                  <div class="ace-settings-item">
-                    <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-rtl" autocomplete="off">
-                    <label class="lbl" for="ace-settings-rtl"> Right To Left (rtl)</label>
-                  </div>
-
-                  <div class="ace-settings-item">
-                    <input type="checkbox" class="ace ace-checkbox-2 ace-save-state" id="ace-settings-add-container" autocomplete="off">
-                    <label class="lbl" for="ace-settings-add-container">
-                      Inside
-                      <b>.container</b>
-                    </label>
-                  </div>
-                </div><!-- /.pull-left -->
-
-                <div class="pull-left width-50">
-                  <div class="ace-settings-item">
-                    <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-hover" autocomplete="off">
-                    <label class="lbl" for="ace-settings-hover"> Submenu on Hover</label>
-                  </div>
-
-                  <div class="ace-settings-item">
-                    <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-compact" autocomplete="off">
-                    <label class="lbl" for="ace-settings-compact"> Compact Sidebar</label>
-                  </div>
-
-                  <div class="ace-settings-item">
-                    <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-highlight" autocomplete="off">
-                    <label class="lbl" for="ace-settings-highlight"> Alt. Active Item</label>
-                  </div>
-                </div><!-- /.pull-left -->
-              </div><!-- /.ace-settings-box -->
-            </div><!-- /.ace-settings-container -->
-
-            <div class="page-header">
-              <h1>Nouvelle Consultation</h1>
-            </div><!-- /.page-header -->
-            <div class="row">
-              <div class="col-xs-12">
-                @include('patient._patientInfo')
-              </div>    
-            </div>
-                <div class="hr hr-18 hr-double dotted"></div>
-
+  <div class="main-content-inner">
+    <div class="page-content">
+      <div class="page-header"> <h1>Nouvelle Consultation</h1></div><!-- /.page-header -->
+        <div class="row">
+          <div class="col-xs-12">
+            @include('patient._patientInfo')
+          </div>    
+        </div>
+            <div class="hr hr-18 hr-double dotted"></div>
             <div class="row">
               <div class="col-xs-12">    
                 <div class="widget-box">
                   <div class="widget-header widget-header-blue widget-header-flat">
                     <h4 class="widget-title lighter">Nouvelle consultation</h4>
-
                     <div class="widget-toolbar">
                       <label>
                         <small class="green">
@@ -292,19 +216,19 @@
                           <ul class="steps" style="margin-left: -1px">
                             <li data-step="1" class="complete">
                               <span class="step">1</span>
-                              <span class="title">Motif (Validation)</span>
+                              <span class="title">Intérrogatoire</span>
                             </li>
                             <li data-step="2" class="active">
                               <span class="step">2</span>
-                              <span class="title">Antécédants</span>
+                              <span class="title">Examens Cliniques</span>
                             </li>
                             <li data-step="3" class="">
                               <span class="step">3</span>
-                              <span class="title">Payment Info</span>
+                              <span class="title">Examen complémentaires</span>
                             </li>
                             <li data-step="4">
                               <span class="step">4</span>
-                              <span class="title">Other Info</span>
+                              <span class="title"></span>
                             </li>
                           </ul>
                         </div>
