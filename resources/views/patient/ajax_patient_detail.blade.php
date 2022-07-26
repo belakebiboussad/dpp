9c123f665cc8f0a636f4e0e27d675d4ddda9e931
belakebi <bbelakebi@cdta.dz>
2022-07-26 12:50:44 +0100

    dfsdf

--- resources/views/patient/ajax_patient_detail.blade.php
+++ resources/views/patient/ajax_patient_detail.blade.php
@@ -125,8 +125,9 @@
 <div class= "center">
   @if( in_array(Auth::user()->role_id,[1,13,14]))
    <a  href="/consultations/create/{{ $patient->id }}" class="btn btn-sm btn-primary btn-create"><i class="ace-icon  fa fa-plus-circle fa-lg bigger-120"></i>Consultation</a>&nbsp;&nbsp;&nbsp;&nbsp;
-  @endif
+  
   <a href="{{ route('rdv.create', ["patient_id"=>$patient->id]) }}" class="btn btn-sm btn-primary btn-create" @if(!isset($patient->Dat_Naissance))  disabled @endif>
       <i class="ace-icon  fa fa-plus-circle fa-lg bigger-120"></i>Rendez-Vous
     </a>
+  @endif
 </div>
