542e1622ed4f27ff1c5cfc5af222461f4320a89d
belakebi <bbelakebi@cdta.dz>
2023-04-06 16:40:46 +0100

    d

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -17,7 +17,7 @@
               </li>
               <li data-step="2">
                 <span class="step">2</span>
-                <span class="title"><i class="ace-icon fa fa-phone"></i> Contact<s/span>
+                <span class="title"><i class="ace-icon fa fa-phone"></i> Contact</span>
               </li>
               <li data-step="3">
                 <span class="step">3</span>
@@ -55,20 +55,89 @@
               </form>
             </div><!-- step-pane -->
             <div class="step-pane" data-step="2">
-              <h3 class="lighter block green">informations</h3>
+            <h3 class="lighter block green">informations</h3>
+            </div><!-- step-pane -->
+            <div class="step-pane" data-step="3">
+            <h3 class="lighter block green">informations</h3>
+            </div><!-- step-pane -->
+            <div class="step-pane" data-step="4">
+            <h3 class="lighter block green">informations</h3>
             </div><!-- step-pane -->
           </div><!-- step-content -->
-        </div><!-- fuelux-wizard contaner -->
-        <hr>
+        </div><!-- fuelux-wizard -->
         <div class="wizard-actions">
-          <button class="btn btn-prev">
-            <i class="ace-icon fa fa-arrow-left"></i>Précédent</button>
-          <button class="btn btn-success btn-next" data-last="Finish">
-            Suivant<i class="ace-icon fa fa-arrow-right icon-on-right"></i>
-          </button>
-        </div>
+<button class="btn btn-prev" disabled="disabled">
+  <i class="ace-icon fa fa-arrow-left"></i>
+  Prev
+</button>
+
+<button class="btn btn-success btn-next" data-last="Finish">
+  Next
+  <i class="ace-icon fa fa-arrow-right icon-on-right"></i>
+</button>
+</div>
       </div>
     </div>
   </div>
 </div>
+@stop
+@section('scripts')
+<script type="text/javascript">
+  jQuery(function() {
+    <script type="text/javascript">
+      jQuery(function($) {
+        var $validation = false;
+        $('#fuelux-wizard-container')
+        .ace_wizard({
+          //step: 2 //optional argument. wizard will jump to step "2" at first
+          //buttons: '.wizard-actions:eq(0)'
+        })
+        .on('actionclicked.fu.wizard' , function(e, info){
+          if(info.step == 1 && $validation) {
+            if(!$('#validation-form').valid()) e.preventDefault();
+          }
+        })
+        //.on('changed.fu.wizard', function() {
+        //})
+        .on('finished.fu.wizard', function(e) {
+          bootbox.dialog({
+            message: "Thank you! Your information was successfully saved!", 
+            buttons: {
+              "success" : {
+                "label" : "OK",
+                "className" : "btn-sm btn-primary"
+              }
+            }
+          });
+        }).on('stepclick.fu.wizard', function(e){
+          //e.preventDefault();//this will prevent clicking and selecting steps
+        });
+        //jump to a step
+        /**
+        var wizard = $('#fuelux-wizard-container').data('fu.wizard')
+        wizard.currentStep = 3;
+        wizard.setState();
+        */
+      
+        //determine selected step
+        //wizard.selectedItem().step
+      
+      
+      
+        //hide or show the other form which requires validation
+        //this is for demo only, you usullay want just one form in your application
+        $('#skip-validation').removeAttr('checked').on('click', function(){
+          $validation = this.checked;
+          if(this.checked) {
+            $('#sample-form').hide();
+            $('#validation-form').removeClass('hide');
+          }
+          else {
+            $('#validation-form').addClass('hide');
+            $('#sample-form').show();
+          }
+        })
+      
+  });
+</script>
 @stop
